ce653c77f9f4841bbdd5b1b51d41c76d566624ce
jasonjiang8866 <jasonjiang8866@gmail.com>
2018-03-06 00:04:35 +0800

    add new file

--- exponentiation_recursive.py
+++ exponentiation_recursive.py
@@ -6,3 +6,11 @@ def exponentiation_recursive(x, e):
         return x*exponentiation_recursive(x,e-1)
 
 print(exponentiation_recursive(4,2))
+
+def exponentiation_iteration(x, e):
+    """Calculates x^e iteratively"""
+    result=1
+    for i in range(1,e+1):
+        result*=x
+    return result
+print(exponentiation_iteration(4,2))
