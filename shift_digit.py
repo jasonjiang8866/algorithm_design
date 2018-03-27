7c2f72567acc3a0ae14d78cb99787a51fa61b938
jasonjiang8866 <jasonjiang8866@gmail.com>
2018-03-27 23:51:08 +0800

    add new file

---
+++ shift_digit.py
@@ -0,0 +1,28 @@
+def shift_one_left(num):
+    remain=num
+    digit=0
+    while remain>10:
+        remain//=10
+        digit+=1
+    return remain+(num-remain*10**digit)*10
+    
+    
+def shift_left(num, n):
+    remain=num
+    digit=0
+    while remain>10**n:
+        remain//=10
+        digit+=1
+    return remain+(num-remain*10**digit)*10**n
+    
+def shift_left_alt(num, n):
+    #your code here
+    if n==0:
+        return num
+    if n==1:
+        return shift_one_left(num)
+    else:
+        return shift_left_alt(shift_left_alt(num, n-1), 1)
+
+
+print(shift_left_alt(12345,2))
