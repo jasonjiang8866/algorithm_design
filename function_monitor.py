665cd1666559697440b9d3afb3cec68f9c3b237d
JYC <36893905+jasonjiang8866@users.noreply.github.com>
2018-05-04 18:37:18 +0800

    extend to multiple input to the monitor function

--- function_monitor.py
+++ function_monitor.py
@@ -14,3 +14,28 @@ def make_monitored(f):
 def double(x):
     return 2 * x
 d = make_monitored(double)
+
+
+def make_monitored_extended(f):
+    counter=[0]
+    def helper(*x):
+        if x==():
+            counter[0]+=1
+            return f()
+        elif x[0]=='how-many-calls?':
+            return counter[0]
+        elif x[0]=='reset-count':
+            counter[0]=0
+        else:
+            counter[0]+=1
+            return f(*x)
+    return helper
+ 
+
+def sum_numbers(*numbers):
+    s = 0
+    for n in numbers:
+        s = s + n
+    return s
+monitored_sum_numbers = make_monitored_extended(sum_numbers)
+
