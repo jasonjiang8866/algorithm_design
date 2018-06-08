dd4bf8225ce0a40aa021a35fe064568026d15552
jasonjiang8866 <jasonjiang8866@gmail.com>
2018-06-08 20:30:11 +0800

    add bubble sort

--- power_set.py
+++ power_set.py
@@ -2,12 +2,23 @@ def power_set(lst):
     if lst==[]:
         return [[]]
     else:
-        item=lst.pop()
-        return power_set(lst[:])+[[item]+x for x in power_set(lst[:])]
+        item=lst[0]
+        return power_set(lst[1:])+[[item]+x for x in power_set(lst[1:])]
 
 
-
+def power_set_check(lst):
+    max_lst=[]
+    for item in lst:
+        if len(item)>len(max_lst):
+            max_lst=item
+    powerset=power_set(max_lst)
+    for item in powerset:
+        if item not in lst:
+            print(item)
+            return False
+    return True
 
 print(
-    (power_set([1,2,3]))
-      )
+power_set_check([[1,2,3],[1,2],[1,3],[2,3],[1],[2],[3],[]])
+    )
+            
