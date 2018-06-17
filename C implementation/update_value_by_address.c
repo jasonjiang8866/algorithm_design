56e7d51bd1d41f3d589373846a7725b22626f762
JYC <36893905+jasonjiang8866@users.noreply.github.com>
2018-06-17 23:17:30 +0800

    add comment

--- C implementation/update_value_by_address.c
+++ C implementation/update_value_by_address.c
@@ -7,6 +7,6 @@ int main()
     b=&a;
     *b+=2;
     printf("a=%d and b=%d\n",a,*b);
-    printf("%c\n",'C' + ('a' - 'A')+1 );
+    printf("%c\n",'C' + ('a' - 'A')+1 );\\bonus...character arithmetics
     return 0;
 }
