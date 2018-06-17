51cd5429b2a9a0e0d69f6e7bc293b1d788242e70
JYC <36893905+jasonjiang8866@users.noreply.github.com>
2018-06-17 23:16:41 +0800

    add new file

---
+++ C implementation/update_value_by_address.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,*b;
+    a=10;
+    b=&a;
+    *b+=2;
+    printf("a=%d and b=%d\n",a,*b);
+    printf("%c\n",'C' + ('a' - 'A')+1 );
+    return 0;
+}
