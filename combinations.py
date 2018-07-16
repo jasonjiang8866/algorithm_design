fae4f25f39b6ed77e86cadb548c1f3eacc65b12c
JYC <36893905+jasonjiang8866@users.noreply.github.com>
2018-07-16 21:01:27 +0800

    update alternative solution

--- combinations.py
+++ combinations.py
@@ -7,3 +7,13 @@ def combinations(lst,n):  # combinations of n elements
             first = lst[0]
             rest = lst[1:]
             return list(map(lambda x: [first]+x, combinations(rest,n-1))) + combinations(rest,n)
+
+
+def combination_alter(lst, n):
+    if len(lst)<=n:
+        return [lst]
+    elif n<=0:
+        return [[]]
+    else:
+        element=lst[0]
+        return combination(lst[1:],n)+ [[element]+x for x in combination(lst[1:],n-1)]
